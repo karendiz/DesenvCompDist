5482790afa813e421db3c87bedbb9ea5e7baf3aa
Karen Diz <71502468+karendiz@users.noreply.github.com>
2021-06-25 13:08:38 -0300

    Update settings.py

--- OAuth2.0/oauthproject/settings.py
+++ OAuth2.0/oauthproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w-a6z1!t+ljbl2*n2)_@$zcw@f)lc+gj3#&^0s@t49kqln$fav'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
